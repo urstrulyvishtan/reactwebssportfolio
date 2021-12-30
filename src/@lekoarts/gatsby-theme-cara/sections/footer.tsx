2ba0c53f37783e4d27d0399c605663bf5464fa57
Sibi Vishtan <64768616+urstrulyvishtan@users.noreply.github.com>
2021-12-30 13:12:14 +0530

    Create footer.tsx

---
+++ src/@lekoarts/gatsby-theme-cara/sections/footer.tsx
@@ -0,0 +1,59 @@
+/** @jsx jsx */
+import { Box, Flex, Link, useColorMode, jsx } from "theme-ui"
+
+const Footer = () => {
+  const [colorMode, setColorMode] = useColorMode()
+  const isDark = colorMode === `dark`
+  const toggleColorMode = (e: any) => {
+    setColorMode(isDark ? `light` : `dark`)
+  }
+
+  return (
+    <Box as="footer" variant="footer">
+      <button
+        sx={{ variant: `buttons.toggle`, fontWeight: `semibold`, display: `block`, mx: `auto`, mb: 3 }}
+        onClick={toggleColorMode}
+        type="button"
+        aria-label="Toggle dark mode"
+      >
+        {isDark ? `Light` : `Dark`}
+      </button>
+      
+      <br />
+      <Flex
+        sx={{
+          justifyContent: `center`,
+          alignItems: `center`,
+          mt: 3,
+          color: `text`,
+          fontWeight: `semibold`,
+          a: { color: `text` },
+        }}
+      >
+        {isDark ? (
+          "Thank You!"
+        ) : (
+           "Thank You!"
+        )}
+        {` `}
+        <Link
+          aria-label="Link to the theme's GitHub repository"
+          sx={{ ml: 2 }}
+          href="https://github.com/urstrulyvishtan/reactweb"
+        >
+          Designed
+        </Link>
+        <div sx={{ mx: 1 }}>by</div>
+        {` `}
+        <Link
+          aria-label="Link to the theme author's website"
+          href="https://medium.com/@sibi.vishtan"
+        >
+          vis7tan
+        </Link>
+      </Flex>
+    </Box>
+  )
+}
+
+export default Footer
